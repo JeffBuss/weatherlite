16624b41e97e2ddd232e71b80fca3e0ca6826483
JeffBuss <turbocapslock@gmail.com>
2016-12-11 17:44:09 -0700

    Clean up general indentation/code style issues

--- lib/components/App.jsx
+++ lib/components/App.jsx
@@ -3,71 +3,73 @@ const ReactDOM = require('react-dom');
 const $ = require('jquery');
 
 
-class App extends React.Component{
- constructor(){
-   super()
-   this.state = {
-     location: '',
-     weather: null,
-   }
- }
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      location: '',
+      weather: null,
+    };
+  }
 
- componentDidMount() {
-    this.setState({location: localStorage.getItem('location' || '')}, () => this.locationAccepted());
- }
+  componentDidMount() {
+    this.setState({ location: localStorage.getItem('location' || '') }, () =>
+    this.locationAccepted());
+  }
 
- locationAccepted(e){
-   $.get(this.props.source + '/' + this.state.location, (data)=>{
-     this.setState({weather:data.slice(0, 7)}, localStorage.setItem('location', this.state.location))
-   })
- }
+  locationAccepted(e) {
+    $.get(this.props.source + '/' + this.state.location, (data) => {
+      this.setState({ weather: data.slice(0, 7) },
+      localStorage.setItem('location', this.state.location));
+    });
+  }
 
- render(){
-   return(
-     <div>
-       <input placeholder='location'
-              value = {this.state.location}
-              onChange = { (event) => {this.setState({location: event.target.value})} }
-       />
-       <input type ='submit'
-              onClick = { (e) => this.locationAccepted(e) }
-       />
-       <input type ='submit' />
-     <WeatherCards weather={ this.state.weather } />
-     </div>
-   )
- }
+  render() {
+    return (
+      <div>
+        <input
+          placeholder='location'
+          value = {this.state.location}
+          onChange = { (e) => { this.setState({ location: e.target.value }); } }
+         />
+         <input type ='submit'
+                onClick = { (e) => this.locationAccepted(e) }
+         />
+         <input type ='submit' />
+       <WeatherCards weather={ this.state.weather } />
+       </div>
+     );
+  }
 }
 
 const WeatherCards = (props) => {
- let { weather } = props
- if(!weather){
-   return(
-     <div>please enter a city</div>
- )} else {
- return(
-   <div className="WeatherCard">
-     { weather.map((card) => <div key={card.date}>
-       <Weather {...card}/>
-       <li>high: {card.temp.high}</li>
-       <li>low: {card.temp.low}</li>
-     </div>
+  let { weather } = props;
+  if (!weather) {
+    return (
+      <div>please enter a city</div>
+ ); }
+  return (
+    <div className="WeatherCard">
+      { weather.map((card) => <div key={card.date}>
+        <Weather {...card}/>
+        <li>high: {card.temp.high}</li>
+        <li>low: {card.temp.low}</li>
+    </div>
      )}
    </div>
- )}
-}
+ );
+};
 
 const Weather = (props) => {
- let {location, date, temp} = props
- return(
-   <div>
-     <article>
-       {location} {date}
-     </article>
-   </div>
- )
-}
+  let { location, date, temp } = props;
+  return (
+    <div>
+      <article>
+        {location} {date}
+      </article>
+    </div>
+ );
+};
 
-
-//denver, san-diego, san-francisco, castle-rock
-ReactDOM.render(<App source= 'https://weatherly-api.herokuapp.com/api/weather' />, document.getElementById('application'))
+// denver, san-diego, san-francisco, castle-rock
+ReactDOM.render(<App source= 'https://weatherly-api.herokuapp.com/api/weather' />, document.getElementById('application'));
